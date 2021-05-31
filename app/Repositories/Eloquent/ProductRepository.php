4158aa7ce04e95c1351307faabde10047698d799
Zarna0x <justnightmare123@gmail.com>
2021-05-31 03:57:16 -0700

    create product repository and its interface

---
+++ app/Repositories/Eloquent/ProductRepository.php
@@ -0,0 +1,32 @@
+<?php
+
+ namespace App\Repositories\Eloquent;
+
+ use App\Repositories\ProductRepositoryInterface;
+ use App\Repositories\Eloquent\Base\BaseRepository;
+
+ class ProductRepository extends BaseRepository implements ProductRepositoryInterface
+ {
+
+     public function getData(Request $request)
+     {
+	 
+     }
+
+     public function update($id, Request $request)
+     {
+	 
+     }
+
+     public function store(Request $request)
+     {
+	 
+     }
+
+     public function delete($id)
+     {
+	 
+     }
+
+ }
+ 
