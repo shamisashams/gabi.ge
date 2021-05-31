b28a985e0d484ce9fd5b074fb6e11edd126b3f67
Zarna0x <justnightmare123@gmail.com>
2021-05-31 03:48:50 -0700

    Products controller CRUD methods

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -1,13 +1,42 @@
 <?php
 
-namespace App\Http\Controllers\Admin;
+ namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+ use App\Http\Controllers\Controller;
+ use Illuminate\Http\Request;
 
-class ProductController extends Controller
-{
-    public function index(){
-        return view('admin.modules.product.index');
-    }
-}
+ class ProductController extends Controller
+ {
+
+     public function index()
+     {
+	 return view('admin.modules.product.index');
+     }
+
+     public function store()
+     {
+	 
+     }
+
+     public function create()
+     {
+	 
+     }
+
+     public function show()
+     {
+	 
+     }
+
+     public function edit()
+     {
+	 
+     }
+
+     public function destory()
+     {
+	 
+     }
+
+ }
+ 
