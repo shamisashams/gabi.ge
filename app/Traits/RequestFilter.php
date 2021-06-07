c92a61343bdb87eb094de66bc8d542de6c7224e2
Zarna0x <justnightmare123@gmail.com>
2021-06-07 04:28:41 -0700

    Add RequestFilter trait for search functionality

---
+++ app/Traits/RequestFilter.php
@@ -0,0 +1,44 @@
+<?php
+
+ namespace App\Traits;
+
+ use Illuminate\Database\Eloquent\Builder;
+ use Illuminate\Http\Request;
+ 
+
+ trait RequestFilter
+ {
+
+     public function setFiltersFromRequest(Builder $modelQueryBuilder, Request $request)
+     {
+	 if ($request['id']) {
+	     $modelQueryBuilder->where('id', '=', (int) $request['id']);
+	 }
+
+	 if (false === is_null($request['status'])) {
+	     $modelQueryBuilder->where('status', '=', (int) $request['status']);
+	 }
+
+	 if ($request['title']) {
+	     $modelQueryBuilder->whereHas('availableLanguage', function ($query) use ($request) {
+		 $query->where('title', 'like', "%{$request['title']}%");
+	     });
+	 }
+
+	 if ($request['slug']) {
+	     $modelQueryBuilder->whereHas('availableLanguage', function ($query) use ($request) {
+		 $query->where('slug', 'like', "%{$request['slug']}%");
+	     });
+	 }
+
+	 if ($request['description']) {
+	     $modelQueryBuilder->whereHas('availableLanguage', function ($query) use ($request) {
+		 $query->where('description', 'like', "%{$request['description']}%");
+	     });
+	 }
+
+	 return $modelQueryBuilder;
+     }
+
+ }
+ 
