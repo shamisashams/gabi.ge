79c072d9fddff4cfbdac49cbd75695ece24ef288
Alex Zhukov <alex3dfree@gmail.com>
2022-05-23 16:58:17 +0400

    update c356

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="facebook-domain-verification" content="6ztqz12hxgejgtyjugys9wdvdlqrci" />
     <meta name="description" content="@yield('description')">
 
     <meta name="language" content="{{app()->getLocale()}}">
