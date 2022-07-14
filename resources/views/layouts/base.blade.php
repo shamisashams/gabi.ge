26d84c80d4f614ca2ccf8f00ea3ec1301040853d
Alex Zhukov <alex3dfree@gmail.com>
2022-07-14 15:30:37 +0400

    translation

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -22,7 +22,23 @@
         href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"
     />
     <link href="{{asset('../css/style.css?v=2')}}" rel="stylesheet">
-
+    <!-- Meta Pixel Code -->
+    <script>
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '658907368990074');
+        fbq('track', 'PageView');
+    </script>
+    <noscript><img height="1" width="1" style="display:none"
+                   src="https://www.facebook.com/tr?id=658907368990074&ev=PageView&noscript=1"
+        /></noscript>
+    <!-- End Meta Pixel Code -->
 
     <script>
 
