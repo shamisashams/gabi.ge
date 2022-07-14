ee8f1a0fa0a85bd93dc02b45b5492a593a457f60
Alex Zhukov <alex3dfree@gmail.com>
2022-07-14 10:48:44 +0400

    translation

--- resources/views/pages/single-blog/index.blade.php
+++ resources/views/pages/single-blog/index.blade.php
@@ -105,7 +105,7 @@
                         var js, fjs = d.getElementsByTagName(s)[0];
                         if (d.getElementById(id)) return;
                         js = d.createElement(s); js.id = id;
-                        js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0";
+                        js.src = "https://connect.facebook.net/ka_GE/sdk.js#xfbml=1&version=v3.0";
                         fjs.parentNode.insertBefore(js, fjs);
                     }(document, 'script', 'facebook-jssdk'));</script>
 
